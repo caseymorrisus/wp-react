a03852d30d9ee1c548a673c587316df4199eebf2
Casey Morris <caseymorrisus@gmail.com>
2017-11-09 12:42:20 -0800

    fixed settings

--- functions.php
+++ functions.php
@@ -4,23 +4,6 @@ if ( ! defined('BUILD_REST__PLUGIN_DIR')) {
   define( 'BUILD_REST__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 }
 
-// Adds WP_REST_ROUTES to JS global
-require_once( BUILD_REST__PLUGIN_DIR . 'includes/build_rest_routes.php' );
-
-// Adds WP_REST_MENUS to JS global
-require_once( BUILD_REST__PLUGIN_DIR . 'includes/build_rest_menus.php' );
-
-// Adds WP_REST_INFO to JS global
-require_once( BUILD_REST__PLUGIN_DIR . 'includes/build_rest_info.php' );
-
-// Adds WP_REST_SETTINGS to JS global
-// called on init to make custom post types available
-function build_settings() {
-  require_once( BUILD_REST__PLUGIN_DIR . 'includes/build_rest_settings.php' );
-}
-
-add_action('init', 'build_settings');
-
 // Create custom post type (Project)
 if ( !function_exists( 'create_projects_type' ) ) {
   function create_projects_type() {
@@ -58,6 +41,23 @@ if ( !function_exists( 'create_works_type' ) ) {
 
 // Add Works post types on init
 add_action('init', 'create_works_type');
+
+// Adds WP_REST_ROUTES to JS global
+require_once( BUILD_REST__PLUGIN_DIR . 'includes/build_rest_routes.php' );
+
+// Adds WP_REST_MENUS to JS global
+require_once( BUILD_REST__PLUGIN_DIR . 'includes/build_rest_menus.php' );
+
+// Adds WP_REST_INFO to JS global
+require_once( BUILD_REST__PLUGIN_DIR . 'includes/build_rest_info.php' );
+
+// Adds WP_REST_SETTINGS to JS global
+// called on init to make custom post types available
+function build_settings() {
+  require_once( BUILD_REST__PLUGIN_DIR . 'includes/build_rest_settings.php' );
+}
+
+add_action('init', 'build_settings');
 
 // Enqueue React bundle script
 wp_enqueue_script('rest-theme-react', BUILD_REST__PLUGIN_DIR . '/build/bundle.js', array(), '1.0.0', true);
