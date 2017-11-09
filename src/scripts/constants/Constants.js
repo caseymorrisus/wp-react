6226b4b8131d1a5acb84c55fa381441ef380bed7
Casey Morris <caseymorrisus@gmail.com>
2017-11-08 16:38:57 -0800

    cleaned up constants

--- src/scripts/constants/Constants.js
+++ src/scripts/constants/Constants.js
@@ -1,4 +1,4 @@
-export const APP_URL = 'http://localhost/wp-react'
-export const API_URL = `${APP_URL}/wp-json/wp/v2`
+export const APP_URL      = 'http://localhost/wp-react'
+export const API_URL      = `${APP_URL}/wp-json/wp/v2`
+export const PATH_PREFIX  = '/wp-react'
 export const TITLE_PREFIX = 'React WP - '
-export const PATH_PREFIX = '/wp-react'
