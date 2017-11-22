1440e622de6716b2ac2c58dd70fe03755750415b
Casey Morris <caseymorrisus@gmail.com>
2017-11-22 13:43:45 -0800

    fixed routes key error

--- src/scripts/routes.js
+++ src/scripts/routes.js
@@ -18,39 +18,23 @@ export const getTemplate = (slug) => {
 export const buildPageRoutes = (routes) => {
   const pages = routes.filter(route => route.type === 'page')
 
-  return pages.map((page, i) => (
-    <Route
-      key={i}
-      component={getTemplate(page.slug)}
-      path={`/${page.slug}`}
-      exact
-    />
-  ))
+  const returned = pages.map((page, i) => ({
+    component: getTemplate(page.slug),
+    path: `/${page.slug}`,
+    exact: true
+  }))
+
+  return returned
 }
 
 export const buildPaginatedRoutes = (postTypes, routesToAdd) => {
-  return [...postTypes, ...routesToAdd].map((name, i) => (
-    <Route
-      key={i}
-      component={getTemplate(name)}
-      path={`/${name}/:page`}
-      exact
-    />
-  ))
-}
+  const returned = [...postTypes, ...routesToAdd].map((name, i) => ({
+    component: getTemplate(name),
+    path: `/${name}/:page`,
+    exact: true
+  }))
 
-export const buildPostSingles = (routes) => {
-  const posts = routes.filter(route => route.type !== 'page')
-  console.log('posts', posts)
-
-  return posts.map((post, i) => (
-    <Route
-      key={i}
-      component={getTemplate(post.type.slice(0, -1))}
-      path={`/${post.type.slice(0, -1)}/${post.slug}`}
-      exact
-    />
-  ))
+  return returned
 }
 
 export const getCustomPostTypes = (postTypes) => {
@@ -65,46 +49,47 @@ export const buildRoutes = (pagesOLD) => {
   const pageRoutes = buildPageRoutes(routes)
 
   // paginated routes eg: /blog/2, /projects/2, /works/3
-  // const postTypes = Object.keys(settings.post_types)
   const postTypes = getCustomPostTypes(settings.post_types)
   const paginatedRoutes = buildPaginatedRoutes(postTypes, ROUTES_TO_ADD)
 
   // post single routes eg: /projects/project-title, /works/works-title, /post/post-title
-  //const singleRoutes = buildPostSingles(routes)
   const singles = ['work', 'project']
-  const singleRoutes = singles.map((single, i) => {
-    return (
-      <Route
-        key={i}
-        component={getTemplate(single)}
-        path={`/${single}/:slug`}
-        exact
-      />
-    )
-  })
 
-  const postSingles = (
+  const singleRoutes = singles.map(single => ({
+    component: getTemplate(single),
+    path: `/${single}/:slug`,
+    exact: true
+  }))
+
+  const postSingles = {
+    component: getTemplate('post'),
+    path: `/post/:slug`,
+    exact: true
+  }
+
+  const allRoutes = [...pageRoutes, ...paginatedRoutes, ...singleRoutes, postSingles]
+
+  const routeComponents = allRoutes.map((route, i) => (
     <Route
-      component={getTemplate('post')}
-      path={`/post/:slug`}
-      exact
+      key={i}
+      component={route.component}
+      path={route.path}
+      exact={route.exact}
     />
-  )
+  ))
 
-  return [...pageRoutes, ...paginatedRoutes, ...singleRoutes, postSingles]
+  return routeComponents
 }
 
-export const getRoutes = (pages) => {
-  return (
-    <Router basename={WPReact.PATH_PREFIX}>
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/" component={templates.home} exact />
-          {buildRoutes(pages)}
-          <Route render={() => { return <Redirect to="/" />}}/>
-        </Switch>
-      </div>
-    </Router>
-  )
-}
+export const getRoutes = (pages) => (
+  <Router basename={WPReact.PATH_PREFIX}>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={templates.home} exact />
+        {buildRoutes(pages)}
+        <Route render={() => { return <Redirect to="/" />}}/>
+      </Switch>
+    </div>
+  </Router>
+)
